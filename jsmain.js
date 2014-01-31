d45ce547e0acc3a62850a3deae7af9d9758ae82c
arvindayal <arvinddayal@gmail.com>
2014-01-31 16:34:57 -0700

    design, delete, and form show/hide functioning.  Adding events not functioning.

--- jsmain.js
+++ jsmain.js
@@ -1,9 +1,5 @@
 $(function() {
-
-
-
-
-
+//Global Functions - Retrieve/create date
 	//Gets Date (Weekday, Month Date, Year)
 	var makeDate = function() {
 		var newD = new Date ();
@@ -17,16 +13,49 @@ $(function() {
 		return displayDate;
 	};
 
+	//Creates new Event-Container with info from form
+	var newEvent = function() {
+		var newTimeInput = $('#timebox').val();
+		var newEventInput = $('#eventbox').val();
+		var newTime = $('<li class="time">');
+		var newEvent = $('<li class="event">');
+		var newDelete = $('<li class="delete">');
+		var newDeleteButton = $('<button type="button" class="button" id="delete">Delete Event</>');
+		var newDeleteLi = newDelete.append(newDeleteButton);
+		var newEventOl = $('<ol class="new-event">').append(newTime).append(newEvent).append(newDeleteLi);
+		$('.time').text(newTimeInput);
+		$('.event').text(newEventInput);
+		return newEventOl;
+	};
+
+
+//Global Functions - Make visible changes to calendar
 	//Pushes date into .date h2
-	$('h2').text(makeDate);
+	$('#date-header').text(makeDate);
+
 	//Click on date toggles display of events
-	$(document).on('click', '.date', function() {
+	$(document).on('click', '#date-header', function() {
 		$(this).next('.event-container').toggle('display');
 	});
 
+	//delete button deletes time/event
+	$(document).on('click', '#delete', function() {
+		$(this).parent().parent().remove();
+	});
 
+	//shows input form
+	$(document).on('click', "#show-form", function() {
+		$(this).parent().next('.submit-form').toggle('display');
+	});
 
-
+	//Add event creates new event from form, clears form, hides form
+	$(document).on('click', '#add-event', function(e) {
+		e.preventDefault();
+		$(this).parent().parent().prev().prepend(newEvent);
+		$(this).parent().parent('.submit-form').toggle('display');
+		$(this).prev('input').val("");
+		$(this).prev().prev('input').val("");
+	});
 
 
 
